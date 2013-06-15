9d252233818c4015837c51fab4a99e93f16884a0
Karthik Sridhar <karthik.s70@gmail.com>
2013-06-14 19:49:26 -0700

    EdWave

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,15 +1,19 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+
+  let (:base_title) {"EdWave"}
+
   describe "Home page" do
-    it "should have the h1 'Sample App'" do
+    it "should have the h1 'EDWAVE'" do
       visit '/static_pages/home'
-      page.should have_selector('h1', :text => 'Sample App')
+      page.should have_selector('h1', :text => 'EDWAVE')
     end
 
     it "should have the title 'Home'" do
 	    visit '/static_pages/home'
-	    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Home")
+	    page.should have_selector('title', :text => "#{base_title} | Home")
     end
   end
 
@@ -17,12 +21,12 @@ describe "StaticPages" do
   describe "Help page" do
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', :text => 'Need Help?')
     end
 
     it "should have the title 'Help'" do
 	    visit '/static_pages/help'
-	    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Help")
+	    page.should have_selector('title', :text => "#{base_title} | Help")
     end  
   end
 
@@ -34,7 +38,19 @@ describe "StaticPages" do
 
     it "should have the title 'About Us'" do
 	    visit '/static_pages/about'
-	    page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+	    page.should have_selector('title', :text => "#{base_title} | About Us")
+    end
+  end
+
+    describe "Contact Us" do
+    it "should have the h1 'Contact Us'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact Us')
+    end
+
+    it "should have the title 'Contact Us'" do
+	    visit '/static_pages/contact'
+	    page.should have_selector('title', :text => "#{base_title} | Contact Us")
     end
   end
 
