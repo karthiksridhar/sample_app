871b1f02eb623baaff24ab7fc1457d3754baa831
Karthik Sridhar <karthik.s70@gmail.com>
2013-06-28 17:39:01 -0700

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -24,8 +24,8 @@ end
 	describe "for signed_in users" do
 		let(:user) {FactoryGirl.create(:user)}
 		before do
-			FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
-			FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
+			FactoryGirl.create(:micropost, user: user, content: "Lorem")
+			FactoryGirl.create(:micropost, user: user, content: "Ipsum")
 			sign_in user
 			visit root_path
 		end
@@ -34,6 +34,16 @@ end
 			user.feed.each do |item|
 				page.should have_selector("li##{item.id}", text: item.content)
 			end
+		end
+
+		describe "follower/following counts" do
+			let(:other_user) { FactoryGirl.create(:user) }
+			before do
+				other_user.follow!(user)
+				visit root_path
+			end
+			it { should have_link("0 following", href: following_user_path(user)) }
+			it { should have_link("1 followers", href: followers_user_path(user)) }
 		end
 	end
   end
